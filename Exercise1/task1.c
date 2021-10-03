847d8f298d6858c06db02c6d76b6ae48c912e248
CJGutz <cjgutzkow@gmail.com>
2021-10-03 18:09:42 +0200

    added new task: task1

---
+++ Exercise1/task1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <float.h>
+
+int main(void)
+{
+
+    float ratio = 2.54;
+
+    float inches = 1;
+    printf("Give integer in centimeters ");
+    scanf("%f", &inches);
+
+    float centimeters = inches * ratio;
+
+    printf("%.2f\n", centimeters);
+
+    return 0;
+}
